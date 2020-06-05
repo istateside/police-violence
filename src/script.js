a48051b576b0830d141abccc89243b3bb6d8546d
Kevin Fleischman <kfleischman@squarespace.com>
2020-06-05 00:01:34 -0400

    update popup to include close trigger and mobile styling

--- src/script.js
+++ src/script.js
@@ -1,24 +1,24 @@
-const popupTrigger = document.getElementById('popup-trigger')
-const popupContainer = document.getElementById('popup-container')
-const popupBackground = document.getElementById('popup-bg')
+const popupTrigger = document.getElementById('js-popup-trigger')
+const popupContainer = document.getElementById('js-popup-container')
+const popupBackground = document.getElementById('js-popup-bg')
+const popupCloser = document.getElementById('js-popup-closer')
 
-function openPopup() {
+function openPopup(e) {
+  e.preventDefault()
   popupContainer.classList.add('is-visible')
 }
 
-function closePopup() {
+function closePopup(e) {
+  e.preventDefault()
   popupContainer.classList.remove('is-visible')
 }
 
-popupTrigger.addEventListener('click', function(e) {
-  e.preventDefault()
-  openPopup()
-})
-
+popupTrigger.addEventListener('click', openPopup)
 popupBackground.addEventListener('click', closePopup)
+popupCloser.addEventListener('click', closePopup)
 
 document.addEventListener('keydown', (keyEvent) => {
   if (keyEvent.code === 'Escape' || keyEvent.keyCode === 27) {
-    closePopup()
+    closePopup(keyEvent)
   }
 })
